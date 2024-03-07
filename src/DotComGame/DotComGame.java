3ba96573e8afe458578d86a0c6e8850a628322b1
Doctor Farz <raffie.lanticse@gmail.com>
2024-03-07 19:42:54 +0800

    Added a Folder DotComGame and files on it not finished

---
+++ src/DotComGame/DotComGame.java
@@ -0,0 +1,43 @@
+package DotComGame;
+
+import java.util.ArrayList;
+
+public class DotComGame {
+     private String name = "";
+
+     public void setName(String comName){
+        name = comName;
+     }
+    ArrayList<Integer> cellLocations = new ArrayList<Integer>();
+    int numOfGuess = 0;
+
+
+    public void setcellLocations(ArrayList<Integer> locations){
+        cellLocations = locations;
+    }
+
+    public String checkYourself(String userGuess){
+       String result = "miss";
+       int numGuess = Integer.parseInt(userGuess);
+       
+       for(int cell: cellLocations){
+            if(cell == numGuess){
+                cellLocations.remove(cellLocations.indexOf(cell));
+                result = "hit";
+                break;
+            }
+       }
+
+       if(cellLocations.size()  == 0){
+        result = "kill";
+      
+       }
+       System.out.println(result);
+       numOfGuess++;
+
+       if(result == "kill"){
+        System.out.println("You took " + numOfGuess + " guesses.");
+       }
+        return result;
+    }
+}
